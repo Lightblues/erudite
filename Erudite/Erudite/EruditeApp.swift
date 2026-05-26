9e4b94f84163ac4cffb2a2377ab89225f4b5f576
easonsshi <easonsshi@tencent.com>
2026-05-26 12:19:43 +0800

    init xocde proj

---
+++ Erudite/Erudite/EruditeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EruditeApp.swift
+//  Erudite
+//
+//  Created by Frank Shi on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct EruditeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
